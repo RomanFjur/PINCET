62096322379075a4352e8b5c802847e68a11b48f
Roman <www.tolkoya@gmail.com>
2020-06-29 21:11:39 +0300

    Создание папки проекта с репозиторием
    Тестовое задание

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+const gulp = require('gulp'),
+      sass = require('gulp-sass'),
+      plumber = require('gulp-plumber'),
+      postcss = require('gulp-postcss'),
+      autoprefixer = require('autoprefixer'),
+      imagemin = require('gulp-imagemin'),
+      webp = require('gulp-webp'),
+      server = require('browser-sync').create();
+
+/* Для sass */
+gulp.task('compile-sass', function () {
+  gulp.src('./website/sass/style.sass')
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest('./dist/css/'))
+    .pipe(server.stream())
+    .pipe(minify())
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('./dist/css/'));
+});
+
+gulp.task('server', function () {
+    server.init({
+        server: './website/'
+    });
+    gulp.watch('./website/sass/*.sass', gulp.parallel('compile-sass'));
+    gulp.watch('./website/*.html').on('change', server.reload);
+});
+
+/* Task to watch less changes */
+gulp.task('watch-sass', function () {
+  gulp.watch('./website/sass/**/*.sass', gulp.parallel('compile-sass'));
+});
+
+/* Task when running `gulp` from terminal */
+gulp.task('default', gulp.parallel('watch-sass', 'server'));
+
+gulp.task('images', function () {
+  return gulp.src('./img/**/*.{png,jpg,svg}')
+  .pipe(imagemin([
+    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.jpegtran({progressive: true}),
+    imagemin.svgo()
+  ]))
+  .pipe(gulp.dest('./website/img'));
+});
+
+gulp.task('webp', function () {
+  return gulp.src('./website/img/**/*.{png,jpg}')
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest('./website/img'));
+});
